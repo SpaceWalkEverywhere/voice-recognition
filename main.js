ec2695296a22d0423eaaa5a903dd81b48dfb3d97
SpaceWalkEverywhere <74199628+SpaceWalkEverywhere@users.noreply.github.com>
2021-04-07 18:00:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var speechrec = window.webkitSpeechRecognition;
+var rec = new speechrec();
+function start(){
+    document.getElementById("textbox").innerHTML="";
+    rec.start();
+}
+rec.onresult=function(event){
+    console.log(event);
+    var content=event.results[0][0].transscript;
+    console.log(content);
+    document.getElementById("textbox").innerHTML="content";
+}
