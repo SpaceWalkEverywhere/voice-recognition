d8ef3db743eee55bc092d4a4f765aef69068290e
SpaceWalkEverywhere <74199628+SpaceWalkEverywhere@users.noreply.github.com>
2021-04-12 17:49:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ rec.onresult=function(event){
     var content=event.results[0][0].transcript;
     console.log(content);
     document.getElementById("textbox").innerHTML=content;
-    speak();
+    if (content == "take my selfie"){
+        speak();
+    }
 }
 function speak(){
     var API=window.speechSynthesis;
@@ -24,11 +26,37 @@ function speak(){
     var utter=new SpeechSynthesisUtterance(speakdata1);
     API.speak(utter);
     Webcam.attach(camera);
+    var n=1;
+    for (var i=0;i<=5;i++){
+        setTimeout(function(){
+            n++;
+        utterance=new SpeechSynthesisUtterance(n);
+        API.speak(utterance);
+        },1000);
+    }
+    setTimeout(function(){
+        snapshot();
+        save();
+    },5000);
+    
 }
 camera=document.getElementById("camera");
+
 Webcam.set({
     width:360,
     height:250,
     image_format:"png",
-    png_quality:100
+    png_quality:90
 })
+function snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie" src="'+data_uri+'"/>';
+    })
+}
+function save(){
+    link=document.getElementById("link");
+    isrc=document.getElementById("selfie").src;
+    link.href=isrc;
+    link.click();
+
+}
